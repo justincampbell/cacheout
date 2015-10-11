d5b4fc61f3f6b81a7a8784dcaaab81bc9b7528cb
Justin Campbell <justin@justincampbell.me>
2015-10-11 10:57:51 -0400

    Proof of concept passthrough command output

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
+	command := flag.Args()
+	bin, args := command[0], command[1:]
+
+	cmd := exec.Command(bin, args...)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(string(output))
 }
