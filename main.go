407f47eb15f5d09462e3f6499ab446bbbafd2003
Justin Campbell <justin@justincampbell.me>
2015-10-13 21:41:00 -0400

    Use cache/file in proof of concept

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
+	"time"
+
+	"github.com/justincampbell/cacheout/cache"
 )
 
 func init() {
@@ -13,14 +19,45 @@ func init() {
 
 func main() {
 	command := flag.Args()
-	bin, args := command[0], command[1:]
+	key := hashCommand(command)
+	ttlArg, bin, args := command[0], command[1], command[2:]
 
-	cmd := exec.Command(bin, args...)
-
-	output, err := cmd.CombinedOutput()
+	ttl, err := time.ParseDuration(ttlArg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print(string(output))
+	fc := cache.NewFileCache(key, &ttl)
+
+	if fc.Stale() {
+		cmd := exec.Command(bin, args...)
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = os.Stdout.Write(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fc.Write(output)
+		fc.Persist()
+	} else {
+		_, err = os.Stdout.Write(fc.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func hashCommand(command []string) string {
+	h := md5.New()
+
+	for _, part := range command {
+		io.WriteString(h, part)
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
